e500f25433460593db26e07c22ea6b2fae8e3a9f
Andrew <61750301+Andrew96-eng@users.noreply.github.com>
2020-06-08 13:43:38 +0200

    Update Constants.cs

--- AnorocMobileApp/AnorocMobileApp/Models/Constants.cs
+++ AnorocMobileApp/AnorocMobileApp/Models/Constants.cs
@@ -9,16 +9,10 @@ namespace AnorocMobileApp.Models
         public static bool IsDev = true;
         public static readonly string GoogleClientID = "googleID on google project .apps.googleusercontent.com";
         public static readonly string GoogleClientSecret = "google secret on google project";
-       //public static readonly string FaceBookDevelpmentHash = "ga0RGNYHvNM5d0SLGQfpQWAPGJ8=";
+       
         public static readonly string FacebookAppID = "985395151878298";
     }
 
 }
 
 
-/*
- *  keystore: codesummore2020
- *  
- *  Generating Realse Hash Key (Facebook): 
- *  > keytool -exportcert -alias YOUR_RELEASE_KEY_ALIAS -keystore YOUR_RELEASE_KEY_PATH | openssl sha1 -binary | openssl base64
- */
