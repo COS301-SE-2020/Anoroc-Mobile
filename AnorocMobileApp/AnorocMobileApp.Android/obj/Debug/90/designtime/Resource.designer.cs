bd634e4066a54539ed907321e5d2db06379b8e4c
Andrew <61750301+Andrew96-eng@users.noreply.github.com>
2020-06-12 16:40:07 +0200

    Added packages

--- AnorocMobileApp/AnorocMobileApp.Android/obj/Debug/90/designtime/Resource.designer.cs
+++ AnorocMobileApp/AnorocMobileApp.Android/obj/Debug/90/designtime/Resource.designer.cs
@@ -13063,37 +13063,40 @@ namespace AnorocMobileApp.Droid
 			public const int fb_login_protocol_scheme = 2131558488;
 			
 			// aapt resource value: 0x7F0D0059
-			public const int hide_bottom_view_on_scroll_behavior = 2131558489;
+			public const int gcm_fallback_notification_channel_label = 2131558489;
 			
 			// aapt resource value: 0x7F0D005A
-			public const int messenger_send_button_text = 2131558490;
+			public const int hide_bottom_view_on_scroll_behavior = 2131558490;
 			
 			// aapt resource value: 0x7F0D005B
-			public const int mtrl_chip_close_icon_content_description = 2131558491;
+			public const int messenger_send_button_text = 2131558491;
 			
 			// aapt resource value: 0x7F0D005C
-			public const int password_toggle_content_description = 2131558492;
+			public const int mtrl_chip_close_icon_content_description = 2131558492;
 			
 			// aapt resource value: 0x7F0D005D
-			public const int path_password_eye = 2131558493;
+			public const int password_toggle_content_description = 2131558493;
 			
 			// aapt resource value: 0x7F0D005E
-			public const int path_password_eye_mask_strike_through = 2131558494;
+			public const int path_password_eye = 2131558494;
 			
 			// aapt resource value: 0x7F0D005F
-			public const int path_password_eye_mask_visible = 2131558495;
+			public const int path_password_eye_mask_strike_through = 2131558495;
 			
 			// aapt resource value: 0x7F0D0060
-			public const int path_password_strike_through = 2131558496;
+			public const int path_password_eye_mask_visible = 2131558496;
 			
 			// aapt resource value: 0x7F0D0061
-			public const int search_menu_title = 2131558497;
+			public const int path_password_strike_through = 2131558497;
 			
 			// aapt resource value: 0x7F0D0062
-			public const int status_bar_notification_info_overflow = 2131558498;
+			public const int search_menu_title = 2131558498;
 			
 			// aapt resource value: 0x7F0D0063
-			public const int title_activity_webview = 2131558499;
+			public const int status_bar_notification_info_overflow = 2131558499;
+			
+			// aapt resource value: 0x7F0D0064
+			public const int title_activity_webview = 2131558500;
 			
 			static String()
 			{
