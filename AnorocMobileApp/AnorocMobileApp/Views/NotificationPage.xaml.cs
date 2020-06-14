7871b3fda007b479f0ac2fbbea5ecaafbc281e8c
Kevin-D-H <u15026681@tuks.co.za>
2020-06-14 11:20:40 +0200

    Merging Notification to Development

---
+++ AnorocMobileApp/AnorocMobileApp/Views/NotificationPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnorocMobileApp.Views
+{
+    public partial class NotificationPage : ContentPage
+    {
+        public NotificationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SendNotification(object sender, EventArgs e)
+        {
+            DependencyService.Get<INotification>().CreateNotification("Anoroc", message.Text);
+        }
+    }
+}
