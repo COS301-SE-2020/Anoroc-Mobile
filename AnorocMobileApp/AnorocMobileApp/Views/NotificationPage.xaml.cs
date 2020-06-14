4ac30bc7fb0be1e5e49d41af4d03f09a8e820265
Kevin-D-H <u15026681@tuks.co.za>
2020-06-14 15:51:57 +0200

    Added API Notification

--- AnorocMobileApp/AnorocMobileApp/Views/NotificationPage.xaml.cs
+++ AnorocMobileApp/AnorocMobileApp/Views/NotificationPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -16,7 +18,32 @@ namespace AnorocMobileApp.Views
 
         private void SendNotification(object sender, EventArgs e)
         {
-            DependencyService.Get<INotification>().CreateNotification("Anoroc", message.Text);
+            
+            getRequestAsync();
         }
+
+        public async void getRequestAsync()
+        {
+     
+            var url = "https://10.0.2.2:5001/notification/all";
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // Pass the handler to httpclient(from you are calling api)
+            HttpClient client = new HttpClient(clientHandler);
+
+            var response = await client.GetAsync(url);
+
+
+            string result = response.Content.ReadAsStringAsync().Result;
+
+        
+           DependencyService.Get<INotification>().CreateNotification("Anoroc", result);
+            
+
+          
+
+        }
+
     }
 }
