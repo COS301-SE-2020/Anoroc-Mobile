60a70f95fcd77d82134057eaa46e6f36f0caca9a
Tebogo <tn.selahle@gmail.com>
2020-07-15 11:15:29 +0200

    Created ContagionContact Model

---
+++ AnorocMobileApp/AnorocMobileApp/Models/ContagionContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AnorocMobileApp.ViewModels;
+using Xamarin.Forms.Internals;
+
+namespace AnorocMobileApp.Models
+{
+    [Preserve(AllMembers = true)]
+    public class ContagionContact : BaseViewModel
+    {
+        #region Public properties
+
+        public Location Location { get; set; }
+
+        public DateTime DateTime { get; set; } 
+        #endregion
+    }
+}
