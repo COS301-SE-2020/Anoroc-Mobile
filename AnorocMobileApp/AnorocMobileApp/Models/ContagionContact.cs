97f46e3d1f9200f48c1810fef97a5f7ef7aeea25
Tebogo <tn.selahle@gmail.com>
2020-07-18 13:31:46 +0200

    Changed Location to string type

--- AnorocMobileApp/AnorocMobileApp/Models/ContagionContact.cs
+++ AnorocMobileApp/AnorocMobileApp/Models/ContagionContact.cs
@@ -4,13 +4,22 @@ using Xamarin.Forms.Internals;
 
 namespace AnorocMobileApp.Models
 {
+    /// <summary>
+    /// Class to contain the location and date of contact with a contagion carrier
+    /// </summary>
     [Preserve(AllMembers = true)]
     public class ContagionContact : BaseViewModel
     {
         #region Public properties
 
-        public Location Location { get; set; }
+        /// <summary>
+        /// string containing the location of contact
+        /// </summary>
+        public string Location { get; set; }
 
+        /// <summary>
+        /// DateTime containing the date and time of contact
+        /// </summary>
         public DateTime DateTime { get; set; } 
         #endregion
     }
