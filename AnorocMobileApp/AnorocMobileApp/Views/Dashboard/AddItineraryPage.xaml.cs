8eb6fd1aa52b66b927568c63bea423521bbf60e0
Tebogo <tn.selahle@gmail.com>
2020-08-06 23:40:53 +0200

    Added SfDatePicker popup

--- AnorocMobileApp/AnorocMobileApp/Views/Dashboard/AddItineraryPage.xaml.cs
+++ AnorocMobileApp/AnorocMobileApp/Views/Dashboard/AddItineraryPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using AnorocMobileApp.DataService;
+﻿using System;
+using AnorocMobileApp.DataService;
+using Syncfusion.XForms.Pickers;
+using Syncfusion.XForms.PopupLayout;
+using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
@@ -11,13 +15,34 @@ namespace AnorocMobileApp.Views.Dashboard
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddItineraryPage
     {
+        private SfPopupLayout popupLayout;
         /// <summary>
         /// Initializes a new instance of the <see cref="AddItineraryPage" /> class.
         /// </summary>
         public AddItineraryPage()
         {
+            this.popupLayout = CreatePopupLayout();
             InitializeComponent();
             this.BindingContext = AddItineraryDataService.Instance.AddItineraryViewModel;
+        }
+
+        private void Clicked(object sender, EventArgs e)
+        {
+            this.popupLayout.Show();
+        }
+
+        private SfPopupLayout CreatePopupLayout()
+        {
+            var popup = new SfPopupLayout();
+            
+            var datePicker = new SfDatePicker()
+            {
+                ShowHeader = false
+            };
+
+            popup.PopupView.ContentTemplate = new DataTemplate(() => datePicker);
+
+            return popup;
         }
     }
 }
