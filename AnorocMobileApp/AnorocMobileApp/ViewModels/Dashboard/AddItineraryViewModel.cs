2b34ff00d3ca3ccdf8ee97409ffd87e4b9491f34
Tebogo <tn.selahle@gmail.com>
2020-08-06 15:18:12 +0200

    Added Add Itinerary page content

---
+++ AnorocMobileApp/AnorocMobileApp/ViewModels/Dashboard/AddItineraryViewModel.cs
@@ -0,0 +1,36 @@
+﻿using AnorocMobileApp.Models.Dashboard;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Xamarin.Forms.Internals;
+
+namespace AnorocMobileApp.ViewModels.Dashboard
+{
+    /// <summary>
+    /// ViewModel for Add Itinerary page.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [DataContract]
+    public class AddItineraryViewModel : BaseViewModel
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance for the <see cref="AddItineraryViewModel"/> class.
+        /// </summary>
+        public AddItineraryViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a collction of value to be displayed in Daily timeline page.
+        /// </summary>
+        [DataMember(Name = "dailyTimeline")]
+        public ObservableCollection<Event> DailyTimeline { get; set; }
+
+        #endregion
+    }
+}
---
+++ AnorocMobileApp/AnorocMobileApp/Views/Dashboard/AddItineraryPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AnorocMobileApp.DataService;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace AnorocMobileApp.Views.Dashboard
+{
+    /// <summary>
+    /// Page to add an itinerary.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddItineraryPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddItineraryPage" /> class.
+        /// </summary>
+        public AddItineraryPage()
+        {
+            InitializeComponent();
+            this.BindingContext = AddItineraryDataService.Instance.AddItineraryViewModel;
+        }
+    }
+}
