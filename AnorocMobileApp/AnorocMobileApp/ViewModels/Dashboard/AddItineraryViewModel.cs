e86f4e2606a0f5189fa522435d72a274f77812f3
Tebogo <tn.selahle@gmail.com>
2020-08-06 23:17:13 +0200

    Added popupLayout with label content

--- AnorocMobileApp/AnorocMobileApp/ViewModels/Dashboard/AddItineraryViewModel.cs
+++ AnorocMobileApp/AnorocMobileApp/ViewModels/Dashboard/AddItineraryViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using AnorocMobileApp.Models.Dashboard;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.Serialization;
 using Xamarin.Forms.Internals;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace AnorocMobileApp.ViewModels.Dashboard
 {
@@ -10,7 +13,7 @@ namespace AnorocMobileApp.ViewModels.Dashboard
     /// </summary>
     [Preserve(AllMembers = true)]
     [DataContract]
-    public class AddItineraryViewModel : BaseViewModel
+    public class AddItineraryViewModel : BaseViewModel, INotifyPropertyChanged
     {
         #region Constructor
 
@@ -19,6 +22,7 @@ namespace AnorocMobileApp.ViewModels.Dashboard
         /// </summary>
         public AddItineraryViewModel()
         {
+            OpenPopupCommand = new Command(OpenPopup);
         }
 
         #endregion
@@ -26,11 +30,57 @@ namespace AnorocMobileApp.ViewModels.Dashboard
         #region Properties
 
         /// <summary>
-        /// Gets or sets a collction of value to be displayed in Daily timeline page.
+        /// Gets or sets a collection of value to be displayed in Daily timeline page.
         /// </summary>
         [DataMember(Name = "dailyTimeline")]
         public ObservableCollection<Event> DailyTimeline { get; set; }
 
         #endregion
+        
+        #region Commands
+        
+        public ICommand OpenPopupCommand { get; set; }
+
+        #endregion
+        
+        #region Methods
+
+        private void OpenPopup()
+        {
+            DisplayPopup = true;
+        }
+
+        #endregion
+        
+        #region Fields
+
+        private bool displayPopup;
+
+        public bool DisplayPopup
+        {
+            get => displayPopup;
+            set
+            {
+                displayPopup = value;
+                RaisePropertyChanged("DisplayPopup");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
     }
 }
