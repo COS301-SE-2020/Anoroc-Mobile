27fc7f7bcc91b7536a5e2b1ec499a840e05c16e0
Tebogo <tn.selahle@gmail.com>
2020-08-09 17:41:52 +0200

    Added service for making API calls to Google Places API

---
+++ AnorocMobileApp/AnorocMobileApp/Services/PlacesService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using AnorocMobileApp.Models;
+
+namespace AnorocMobileApp.Services
+{
+    public class PlacesService
+    {
+        private static HttpClient _httpClientInstance;
+        public static HttpClient HttpClientInstance => _httpClientInstance ?? (_httpClientInstance = new HttpClient());
+        
+        
+    }
+}
