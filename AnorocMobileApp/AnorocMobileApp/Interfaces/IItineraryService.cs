71c6322a78663d56b4a3fc641cffde0583a45fe9
Andrew Wilson <u15191223@tuks.co.za>
2020-08-11 19:10:22 +0200

    Init Itinerary Service

---
+++ AnorocMobileApp/AnorocMobileApp/Interfaces/IItineraryService.cs
@@ -0,0 +1,17 @@
+﻿using AnorocMobileApp.Models;
+using AnorocMobileApp.Models.Itinerary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnorocMobileApp.Interfaces
+{
+    public interface IItineraryService
+    {
+        Dictionary<Location, int> Refresh();
+
+        void ProcessItinerary(Itinerary userItinerary);
+
+        Dictionary<Location, int> GetUserItineraries();
+    }
+}
---
+++ AnorocMobileApp/AnorocMobileApp/Services/ItineraryService.cs
@@ -0,0 +1,27 @@
+﻿using AnorocMobileApp.Interfaces;
+using AnorocMobileApp.Models;
+using AnorocMobileApp.Models.Itinerary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnorocMobileApp.Services
+{
+    class ItineraryService : IItineraryService
+    {
+        public Dictionary<Location, int> GetUserItineraries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessItinerary(Itinerary userItinerary)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<Location, int> Refresh()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
