9bfff96bc6970ca4f4ac20cd300469cb7371aeac
Tebogo <tn.selahle@gmail.com>
2020-10-05 03:37:00 +0200

    Added functionality for location tracking toggle

--- AnorocMobileApp/AnorocMobileApp/Views/Settings/SettingPage.xaml.cs
+++ AnorocMobileApp/AnorocMobileApp/Views/Settings/SettingPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using AnorocMobileApp.Interfaces;
+using Plugin.SecureStorage;
+using Syncfusion.XForms.Buttons;
+using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
@@ -17,6 +20,21 @@ namespace AnorocMobileApp.Views.Settings
         public SettingPage()
         {
             InitializeComponent();
+        }
+
+        private void SfSwitch_OnStateChanged(object sender, SwitchStateChangedEventArgs e)
+        {
+            var backgroundLocationService = App.IoCContainer.GetInstance<IBackgroundLocationService>();
+            if (e.NewValue == true)
+            {
+                CrossSecureStorage.Current.SetValue("Location", "true");
+                backgroundLocationService.Start_Tracking();
+            }
+            else
+            {
+                backgroundLocationService.Stop_Tracking();
+                CrossSecureStorage.Current.SetValue("Location", "false");
+            }
         }
     }
 }
